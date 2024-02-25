25fdbd4c81b61c8b993b0740277958bb87497fe4
Michal Grzymek <mjgrzymek@gmail.com>
2024-02-25 17:04:55 +0100

    remove analytics and comments

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,7 +19,7 @@ const siteMetadata = {
   //threads: 'https://www.threads.net',
   //instagram: 'https://www.instagram.com',
   locale: 'en-US',
-  analytics: {
+  /*analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
@@ -44,14 +44,14 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    //provider: 'buttondown',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    //provider: 'giscus', // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -75,7 +75,7 @@ const siteMetadata = {
       // This corresponds to the `data-lang="en"` in giscus's configurations
       lang: 'en',
     },
-  },
+  },*/
   search: {
     provider: 'kbar', // kbar or algolia
     kbarConfig: {
