607978d5d20b63e883c8416ba75b72e5aa38436b
Michal Grzymek <mjgrzymek@gmail.com>
2024-02-25 17:27:19 +0100

    fix analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,7 +19,7 @@ const siteMetadata = {
   //threads: 'https://www.threads.net',
   //instagram: 'https://www.instagram.com',
   locale: 'en-US',
-  /*analytics: {
+  analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
@@ -40,7 +40,7 @@ const siteMetadata = {
     // googleAnalytics: {
     //   googleAnalyticsId: '', // e.g. G-XXXXXXX
     // },
-  },*/
+  },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
